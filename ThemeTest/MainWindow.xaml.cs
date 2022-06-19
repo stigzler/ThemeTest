a24676a79d1a7139353799ed286614c17a7a483b
Steve Hall <stigzler@msn.com>
2022-06-19 19:17:44 +0100

    Add project files.

---
+++ ThemeTest/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ThemeTest.Resources;
+
+namespace ThemeTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Collection<ResourceDictionary> mergedApplicationDictionaries = Application.Current.Resources.MergedDictionaries;
+            for (int resourceIndex = 0; resourceIndex < mergedApplicationDictionaries.Count; resourceIndex++)
+            {
+                ResourceDictionary resourceDictionary = mergedApplicationDictionaries[resourceIndex];
+
+                // Identify the current active theme file (resource dictionary)
+                if (resourceDictionary.Source == ThemingResources.SquaredThemeResourcesFileUri)
+                {
+                    // Remove the current active theme file (resource dictionary)...
+                    mergedApplicationDictionaries.RemoveAt(resourceIndex);
+
+                    // ... to replace it with the one for the rounded theme
+                    var roundThemeDictionary = new ResourceDictionary() { Source = ThemingResources.RoundThemeResourcesFileUri };
+                    mergedApplicationDictionaries.Add(roundThemeDictionary);
+
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ ThemeTest/Resources/ThemingResources.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ThemeTest.Resources
+{
+    public static class ThemingResources
+    {
+        public static readonly Uri SquaredThemeResourcesFileUri = new Uri("pack://application:,,,/MyProject;component/Resources/SquaredTheme.xaml");
+        public static readonly Uri RoundThemeResourcesFileUri = new Uri("pack://application:,,,/MyProject;component/Resources/RoundTheme.xaml");
+
+        public static ComponentResourceKey ButtonBackgroundBrushKey = new ComponentResourceKey(typeof(ComponentResourceKey), "ButtonBackgroundBrushKey");
+        public static ComponentResourceKey ButtonBackgroundColorKey = new ComponentResourceKey(typeof(ComponentResourceKey), "ButtonBackgroundColorKey");
+        public static ComponentResourceKey ButtonCornerRadiusKey = new ComponentResourceKey(typeof(ComponentResourceKey), "ButtonCornerRadiusKey");
+    }
+}
+
+// Note: CHnaged 'ThemeResourceKeys' to 'ComponentResourceKey'
